d82e71716d82b829463f6e3db1bd0679f48b742f
JayJeong <80833894+JayJeong1741@users.noreply.github.com>
2026-09-15 20:39:34 +0900

    first commit

---
+++ src/main/java/kr/ktb/zura/needu/NeeduBeApplication.java
@@ -0,0 +1,13 @@
+package kr.ktb.zura.needu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeeduBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeeduBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ktb/zura/needu/NeeduBeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ktb.zura.needu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeeduBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
