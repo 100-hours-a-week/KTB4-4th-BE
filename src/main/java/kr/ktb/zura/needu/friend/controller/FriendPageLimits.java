db88343f7f661968b5d2b95a8d46e4d0c1dccf54
jynoh00 <wndus123sh@naver.com>
2026-09-22 19:36:56 +0900

    refactor: 친구 컨트롤러 상수를 도메인 상수 클래스로 분리

---
+++ src/main/java/kr/ktb/zura/needu/friend/controller/FriendPageLimits.java
@@ -0,0 +1,10 @@
+package kr.ktb.zura.needu.friend.controller;
+
+final class FriendPageLimits {
+
+    static final int MIN_PAGE_SIZE = 1;
+    static final int MAX_PAGE_SIZE = 50;
+
+    private FriendPageLimits() {
+    }
+}
---
+++ src/main/java/kr/ktb/zura/needu/friend/controller/FriendResponseMessages.java
@@ -0,0 +1,10 @@
+package kr.ktb.zura.needu.friend.controller;
+
+final class FriendResponseMessages {
+
+    static final String FRIEND_FOUND = "친구 정보를 조회했습니다.";
+    static final String FRIENDS_FOUND = "친구 목록 조회에 성공했습니다.";
+
+    private FriendResponseMessages() {
+    }
+}
